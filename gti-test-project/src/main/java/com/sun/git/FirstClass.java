0fe269870a59d27f6b74f27a968b47b36753920b
ybh <291318009@qq.com>
2020-12-26 21:30:27 +0800

    把本地工程添加到git

---
+++ gti-test-project/src/main/java/com/sun/git/FirstClass.java
@@ -0,0 +1,21 @@
+package com.sun.git;
+
+/**
+ * @author ybh
+ * @Description:
+ * @company： 日光科技
+ * @Email:
+ * @create： 2020-12-26 21:21
+ */
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
